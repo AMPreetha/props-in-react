1d9220b7f27337efe8c899cba02a02858af2052e
AMPreetha <127812727+AMPreetha@users.noreply.github.com>
2023-04-01 10:24:54 +0530

    Add files via upload

--- expenseItem.js
+++ expenseItem.js
@@ -1,14 +1,22 @@
 import './expense.css'
-function ExpenseItem(){
+function ExpenseItem(props){
     const exp = 'Food Rs 10';
     const petrol = "Petrol Rs 100";
     const movie = 'Movies Rs 200';
-    const loc='locationOfExpense'
+    const loc='locationOfExpense';
+    const date=props.date.toLocaleString('en-US',{day:'2-digit'})
+    const month = props.date.toLocaleString('en-US', { month: "long" });
+    const year = props.date.getFullYear
+
     return (
-        
       <div>
         <div>
           <h1 className="expense-item">Expense Item</h1>
+        </div>
+        <div>
+          <p>{date}</p>
+          <p>{month}</p>
+          <p>{year}</p>
         </div>
         <div>
           <p className="food">{exp}</p>
