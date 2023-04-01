be47bbecc87c55816014023534a1e6200bd72333
AMPreetha <127812727+AMPreetha@users.noreply.github.com>
2023-04-01 15:39:26 +0530

    Add files via upload

--- expenseItem.js
+++ expenseItem.js
@@ -1,23 +1,20 @@
 import './expense.css'
-const ExpenseItem=((props)=>{
+const ExpenseItem=(()=>{
+  const clickHandler=()=>{
+    console.log('clicked')
+  }
     const exp = 'Food Rs 10';
     const petrol = "Petrol Rs 100";
     const movie = 'Movies Rs 200';
     const loc='locationOfExpense';
-    const date=props.date.toLocaleString('en-US',{day:'2-digit'})
-    const month = props.date.toLocaleString('en-US', { month: "long" });
-    const year = props.date.getFullYear
+    
 
     return (
       <div>
         <div>
           <h1 className="expense-item">Expense Item</h1>
         </div>
-        <div>
-          <p>{date}</p>
-          <p>{month}</p>
-          <p>{year}</p>
-        </div>
+        
         <div>
           <p className="food">{exp}</p>
         </div>
@@ -28,6 +25,7 @@ const ExpenseItem=((props)=>{
           <p className="movies">{movie}</p>
           <p>{loc}</p>
         </div>
+        <button onClick={clickHandler}>Delete Expense</button>
       </div>
     );
 });
