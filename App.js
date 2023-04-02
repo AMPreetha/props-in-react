b30d4bbbcb482231288235569049718ec59a7370
AMPreetha <127812727+AMPreetha@users.noreply.github.com>
2023-04-02 08:32:28 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
+import NewExpense from "./components/expense/newexpense";
 import ExpenseItem from "./components/expenseItem";
 
 import "./App.css";
-  
 
 function App() {
-  const exp=[{loc:'locationOfExpense'}]
-  for(let i=0;i<100;i++){
-      return exp;
-  }
   return (
     <div className="App">
       <header className="App-header">
-        <ExpenseItem loc={exp[0]}></ExpenseItem>
+        <NewExpense></NewExpense>
+        <ExpenseItem></ExpenseItem>
       </header>
     </div>
   );
